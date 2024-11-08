ffc25c036a4a4c8adde7fa5758665154c2668b1e
Hossein Ghanimati <165092377+hossein-ghanimati@users.noreply.github.com>
2024-11-08 10:11:00 +0330

    Update .js

--- .js
+++ .js
@@ -5,30 +5,11 @@ const getData = (dataName) => {
         case "name":
             data ="Hossein Ghanimati"
             break;
-    
-        case "skills":
-            data ="HTML CSS JS"
-            break;
-
-        case "workSamples":
-            data = [
-                {
-                    name : "asancode training site",
-                    category : "training",
-                    languages : "HTML, CSS, JS",
-                    description : `
-                        An educational site with all facilities, responsive and with a management panel
-                    `,
-                    link : "https://hossein-ghanimati.github.io/asancode"
-                }
-            ]
-            break;
 
     
         case "description":
-            data = `Hi guys. I'm Front End Developer. My skills are HTML, CSS and javascript. I have been in the front-end world for almost a year
-            And I have been dealing with the dear JavaScript language for almost 9 months
-            I have recently mastered Javascript and am about to start learning React`
+            data = `Hi guys. I'm Front End Developer. My skills are HTML, CSS , javascript, tailwind, react, typescript (and some other packages). I have been in the front-end world for almost 1\5 year 
+            \nAnd I have been dealing with the dear JavaScript language for almost 1 year and 2 month \nI have recently mastered react and am about to start learning Next`
             break;
     
         default:
@@ -42,9 +23,7 @@ const getInfo = username => {
     if(username != "hossein-ghanimati") return false;
 
     const info = {
-        name : getData('name'),
-        skills : getData('skills'),
-        workSamples : getData('workSamples'),
+        name : getData('name')
         description : getData("description")
     }
 
